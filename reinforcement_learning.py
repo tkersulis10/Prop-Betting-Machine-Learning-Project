f49470632935561c12a5885068260543053caef4
Tomas Kersulis <kersulistomas1@gmail.com>
2023-04-11 08:56:52 -0400

    Add validation for all hyperparameters

--- reinforcement_learning.py
+++ reinforcement_learning.py
@@ -23,7 +23,7 @@ def convert_to_valid_value(value):
         value = value[1:]
     return float(value)
 
-def train_players(player_list, player_alpha_divider):
+def train_players(player_list, weight_alpha_divider, feature_alpha_divider):
     """
     Trains and returns trained players in player_list. Only returns players
     that played at least 20 games this season.
@@ -44,6 +44,7 @@ def train_players(player_list, player_alpha_divider):
         actual_points = []
         feature_values = []
         num_games = 0
+        opp_team_list = []
         for i in range(82):
             if type(player[i]) != str:
                 # Get team's and opponents stats and inactives
@@ -52,11 +53,14 @@ def train_players(player_list, player_alpha_divider):
                 else:
                     home = False
                 if home == True:
+                    opp_team = team_games[i].away_team
                     opp_team_stats = team_games[i].away_team_stats
                     team_inactives = team_games[i].home_inactives # GSW own inactives
                 else:
+                    opp_team = team_games[i].home_team
                     opp_team_stats = team_games[i].home_team_stats
                     team_inactives = team_games[i].away_inactives # GSW own inactives
+                opp_team_list.append(opp_team)
 
                 # Extract player's gamelog stats as features
                 feature_values_strings = list(player[i].values())
@@ -69,12 +73,6 @@ def train_players(player_list, player_alpha_divider):
                     new_value = convert_to_valid_value(value)
                     feature_values[num_games - 1].append(new_value)
                 
-                # Extract player's team inactives for each game as features
-                inactives_features = [0] * roster_size
-                for team_member in range(roster_size):
-                    if team_roster[team_member] in team_inactives:
-                        inactives_features[team_member] = 1 # set to 1 if player inactive
-
                 # Extract opposing team's game-by-game stats as features
                 opp_team_stat_list = list(opp_team_stats.values())
                 opp_team_features = []
@@ -82,6 +80,12 @@ def train_players(player_list, player_alpha_divider):
                     # Convert stats to valid floats
                     new_opp_value = convert_to_valid_value(opp_value)
                     opp_team_features.append(new_opp_value)
+
+                # Extract player's team inactives for each game as features
+                inactives_features = [0] * roster_size
+                for team_member in range(roster_size):
+                    if team_roster[team_member] in team_inactives:
+                        inactives_features[team_member] = 1 # set to 1 if player inactive
 
                 # Add all features to one list
                 feature_values[num_games - 1] += opp_team_features
@@ -94,28 +98,35 @@ def train_players(player_list, player_alpha_divider):
             weights = [0] * num_features
 
             # Initialize hyperparameters
-            player_alpha = 1 / (num_games * num_features * player_alpha_divider) # learning rate
+            weight_alpha = 1 / (num_games * num_features * weight_alpha_divider) # weight learning rate
+            rolling_feature_sum = feature_values[0][0:num_features - roster_size] # ADJUST IF NEW FEATURES ADDED/DELETED
+            feature_alpha = 1 / (num_games * num_features * feature_alpha_divider) # feature learning rate
 
             # Train weights on first (num_games - 10) games of the player's season
             for game in range(num_games - 10):
                 prediction = 0
                 for feature in range(num_features):
+                    if game > 0 and feature < num_features - roster_size:
+                        rolling_feature_sum[feature] += (feature_alpha * feature_values[game][feature]) - (feature_alpha * rolling_feature_sum[feature])
                     prediction += feature_values[game][feature] * weights[feature]
                 difference = actual_points[game] - prediction
                 for weight in range(num_features):
-                    weights[weight] += player_alpha * difference * feature_values[game][weight]
+                    weights[weight] += weight_alpha * difference * feature_values[game][weight]
 
-            player_feature_dict[player_name] = (feature_values, weights, actual_points)
+            # TO DO: ADD GAMES FROM PREVIOUS SEASONS WITH HYPERPARAMETER TO WEIGH
+            # DOWN PREVIOUS SEASONS/TEAMS
+            player_feature_dict[player_name] = (feature_values, weights, actual_points, rolling_feature_sum, opp_team_list)
     
     # Return dictionary of each player's features, weights, and actual points
     return player_feature_dict
 
-def evaluate_players(player_feature_dict, start_games_from_end, end_games_from_end, step):
+def evaluate_players(player_feature_dict, team_dict, start_games_from_end, end_games_from_end, step):
     """
     Evaluates the predicted points scored for each player in player_feature_dict
     versus their actual points scored from (including) start_games_from_end
     from the end to (excluding) end_games_from_end from the end by using every
-    step game.
+    step game. team_dict is a dictionary from each team to their average stats
+    for the season using an exponential rolling average.
 
     Example: If player played 70 games in the season and you want to evaluate
     the player's last 10 games (indexes 60 - 69), start_games_from_end = 10
@@ -128,9 +139,10 @@ def evaluate_players(player_feature_dict, start_games_from_end, end_games_from_e
         feature_values = player_feature_dict[player][0]
         weights = player_feature_dict[player][1]
         actual_points = player_feature_dict[player][2]
+        rolling_features = player_feature_dict[player][3]
+        opp_team_list = player_feature_dict[player][4]
         num_features = len(feature_values[0])
         num_evaluation_games = math.ceil((start_games_from_end - end_games_from_end) / 2)
-        print(num_evaluation_games)
         start_game = num_games - start_games_from_end
         end_game = num_games - end_games_from_end
 
@@ -139,7 +151,13 @@ def evaluate_players(player_feature_dict, start_games_from_end, end_games_from_e
         real_points = []
         count = 0
         for game in range(start_game, end_game, step):
-            for feature in range(num_features):
+            opposing_team = opp_team_list[game]
+            for feature in range(34):
+                final_prediction[count] += rolling_features[feature] * weights[feature]
+            # OPP TEAM INSERT HERE
+            for feature in range(34, 69):
+                final_prediction[count] += team_dict[opposing_team][feature - 34] * weights[feature]
+            for feature in range(69, num_features):
                 final_prediction[count] += feature_values[game][feature] * weights[feature]
             real_points.append(actual_points[game])
             count += 1
@@ -151,48 +169,75 @@ def evaluate_players(player_feature_dict, start_games_from_end, end_games_from_e
 
     return results
 
-def validation(player_list, random_tests):
+def validation(player_list, team_list, random_tests, weight_alpha=None, feature_alpha=None, opp_team_alpha=None):
     """
     Randomly test different learning rates and initial weights to find and
     return the best performing hyperparameters based on least squares on
     player_list. Tests each hyperparameter on random_tests number of random
-    values. Validates on each players 10th, 8th, 6th, 4th, and 2nd last games
-    of the season.
+    values. Validates on each players 10th, 8th, 6th, 4th, and 2nd to last
+    games of the season.
+
+    You can specify weight_alpha, feature_alpha, and opp_team_alpha when
+    calling the function. The specified hyperparameters will be set and not
+    randomly tested.
     """
     # Generate random number for the divider of alpha (learning rate)
-    random_dividers = []
+    if weight_alpha == None:
+        random_weight_dividers = []
+    else:
+        random_weight_dividers = [weight_alpha]
+    if feature_alpha == None:
+        random_feature_dividers = []
+    else:
+        random_feature_dividers = [feature_alpha]
+    if opp_team_alpha == None:
+        random_opp_team_dividers = []
+    else:
+        random_opp_team_dividers = [opp_team_alpha]
     for i in range(random_tests):
-        random_dividers.append(random.randint(1, 500))
+        if weight_alpha == None:
+            random_weight_dividers.append(random.randint(1, 500))
+        if feature_alpha == None:
+            random_feature_dividers.append(random.randint(1, 500))
+        if opp_team_alpha == None:
+            random_opp_team_dividers.append(random.randint(1, 500))
 
     # Find the most accurate divider
-    best_divider = 1
+    best_dividers = (1, 1, 1)
     best_divider_lss = float('inf')
-    for divider in random_dividers:
-        # Train model using divider
-        player_feature_dict = train_players(player_list, divider)
+    for weight_divider in random_weight_dividers:
+        for feature_divider in random_feature_dividers:
+            for opp_team_divider in random_opp_team_dividers:
+                # Train model using divider
+                player_feature_dict = train_players(player_list, weight_divider, feature_divider)
 
-        # Evaluate model using divider
-        results = evaluate_players(player_feature_dict, 10, 0, 2)
+                # Get exponential rolling average of opposing team stats
+                team_dict = {}
+                for team in team_list:
+                    team_dict[team.name] = get_opp_team_stats(team, opp_team_divider)
 
-        # Check if this divider is the best one so far using least squares sum
-        ls_sum = 0
-        for player in results.keys():
-            ls_sum += abs(results[player][0] - results[player][1]) ** 2
-        
-        if ls_sum < best_divider_lss:
-            best_divider = divider
-            best_divider_lss = ls_sum
+                # Evaluate model using divider
+                results = evaluate_players(player_feature_dict, team_dict, 10, 0, 2)
+
+                # Check if this divider is the best one so far using least squares sum
+                ls_sum = 0
+                for player in results.keys():
+                    ls_sum += abs(results[player][0] - results[player][1]) ** 2
+                
+                if ls_sum < best_divider_lss:
+                    best_dividers = (weight_divider, feature_divider, opp_team_divider)
+                    best_divider_lss = ls_sum
     
-    return best_divider
+    return best_dividers
 
-def test_players(player_feature_dict):
+def test_players(player_feature_dict, team_dict):
     """
-    Test features and weights in player_feature_dict for each player.
-    Tests on each player's 9th, 7th, 5th, 3rd and last games of the season
-    and outputs the model's predicted points vs player's actual points for
-    these games of the player's season.
+    Test features and weights in player_feature_dict for each player with
+    opposing teams' stats in team_dict. Tests on each player's 9th, 7th, 5th,
+    3rd and last games of the season and outputs the model's predicted points
+    vs player's actual points for these games of the player's season.
     """
-    results = evaluate_players(player_feature_dict, 9, 0, 2)
+    results = evaluate_players(player_feature_dict, team_dict, 9, 0, 2)
 
     for player in results.keys():
         # Output the predicted vs average points and write to file
@@ -210,8 +255,19 @@ def get_opp_team_stats(team, learning_rate):
     """
     team_games = team.games
     team_name = team.name
-    average_stats = [0] * 35
-    for game in range(len(team_games)):
+    if team_games[0].home_team == team_name:
+        home = True
+    else:
+        home = False
+    if home == True:
+        average_stats_str = list(team_games[0].home_team_stats.values())
+    else:
+        average_stats_str = list(team_games[0].away_team_stats.values())
+    average_stats = []
+    for value in average_stats_str:
+        new_value = convert_to_valid_value(value)
+        average_stats.append(new_value)
+    for game in range(1, len(team_games)):
         if team_games[game].home_team == team_name:
             home = True
         else:
@@ -230,7 +286,7 @@ def get_opp_team_stats(team, learning_rate):
             new_value = convert_to_valid_value(value)
             team_features.append(new_value)
 
-        alpha = learning_rate
+        alpha = 1 / (82 * 35 * learning_rate)
         for feature in range(len(team_features)):
             average_stats[feature] += (alpha * team_features[feature]) - (alpha * average_stats[feature])
     
@@ -246,26 +302,28 @@ with open('players.pkl', 'rb') as inp:
 gamelog_list = []
 team_list = []
 for player in players:
-   player_var = players[player]
-   if player_var.year == 2022:
-       gamelog_list.append(player_var)
+    player_var = players[player]
+    if player_var.year == 2022:
+        # Add all teams to team_list
+        player_team = player_var.team
+        if player_team not in team_list:
+            team_list.append(player_team)
 
-       # Add all teams to team_list
-       player_team = player_var.team
-       if player_team not in team_list:
-           team_list.append(player_team)
+        if player_var.team.name == "GSW":
+            gamelog_list.append(player_var)
 
-team_stats = {}
-for team in team_list:
-    team_stats[team.name] = get_opp_team_stats(team, 0.0005)
-
-# Find best alpha divider hyperparameter
-best_alpha = validation(gamelog_list, 20)
+# Find best hyperparameters
+best_hyperparameters = validation(gamelog_list, team_list, 20)
 with open("reinforcement_learning_output.txt", "a") as file:
-    file.write("Best divider found: " + str(best_alpha) + "\n")
+    file.write("Best weight alpha found: " + str(best_hyperparameters[0]) + "\n")
+    file.write("Best feature alpha found: " + str(best_hyperparameters[1]) + "\n")
+    file.write("Best opposing team alpha found: " + str(best_hyperparameters[2]) + "\n")
 
 # Train model using best hyperparameter
-player_dict = train_players(gamelog_list, best_alpha)
+team_dict = {}
+for team in team_list:
+    team_dict[team.name] = get_opp_team_stats(team, best_hyperparameters[2])
+player_dict = train_players(gamelog_list, best_hyperparameters[0], best_hyperparameters[1])
 
 # Evaluate model
-test_players(player_dict)
+test_players(player_dict, team_dict)
