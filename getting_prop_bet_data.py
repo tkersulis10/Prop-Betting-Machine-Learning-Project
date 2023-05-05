e12f631f24a6266f41bae00c623d0aa2ba341163
Tomas Kersulis <kersulistomas1@gmail.com>
2023-05-05 08:30:23 -0400

    Get prop bets for pts, asts, rebs for 2022-2023

--- getting_prop_bet_data.py
+++ getting_prop_bet_data.py
@@ -5,74 +5,88 @@ import time
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-# Main URL for prop bets
-main_url = "https://www.bettingpros.com/nba/odds/player-props/points/?date="
+def get_data(stat):
+    # Main URL for prop bets
+    main_url = "https://www.bettingpros.com/nba/odds/player-props/" + stat + "/?date="
 
-# Set initial date
-date_year = 2021
-date_month = 1
-date_day = 6
+    # Set initial date
+    date_year = 2021
+    date_month = 1
+    date_day = 6
 
-# Chrome webdriver options
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
+    # Chrome webdriver options
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
 
-# Different driver paths per computer
-driver_paths = ['/home/tkersulis/cs4701/project/chromedriver.exe', "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"]
-driver = webdriver.Chrome(executable_path=driver_paths[0], options=options)
+    # Different driver paths per computer
+    driver_paths = ['/home/tkersulis/cs4701/project/chromedriver.exe', "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"]
+    driver = webdriver.Chrome(executable_path=driver_paths[0], options=options)
 
-# Open output file
-with open("points_prop_bet_output.txt", "a") as file:
-    # Loop through years, months, and days
-    for year in range(2021, 2023):
-        for month in range(1, 13):
-            # Determine the number of days in the month
-            days_in_month = 31 if month in [1, 3, 5, 7, 8, 10, 12] else (28 if month == 2 else 30)
+    # Open output file
+    file_name = stat + "_prop_bet_output.txt"
+    with open(file_name, "a") as file:
+        # Loop through years, months, and days
+        if stat == "rebounds":
+            start_year = 2022
+        else:
+            start_year = 2023
+        for year in range(start_year, 2024):
+            if year == 2023:
+                end_month = 6
+            else:
+                end_month = 13
+            for month in range(1, end_month):
+                # Determine the number of days in the month
+                days_in_month = 31 if month in [1, 3, 5, 7, 8, 10, 12] else (28 if month == 2 else 30)
 
-            for day in range(1, days_in_month + 1):
-                # Construct the URL for the specific date
-                url = main_url + f"{year}-{month:02d}-{day:02d}"
+                for day in range(1, days_in_month + 1):
+                    # Construct the URL for the specific date
+                    url = main_url + f"{year}-{month:02d}-{day:02d}"
 
-                # Load the URL in the webdriver
-                driver.get(url)
-                time.sleep(2)
+                    # Load the URL in the webdriver
+                    driver.get(url)
+                    time.sleep(2)
 
-                # Scrolling implementation:
-                # Credit: https://medium.com/analytics-vidhya/using-python-and-selenium-to-scrape-infinite-scroll-web-pages-825d12c24ec7
-                scrolling_wait = 1
-                website_height = driver.execute_script("return window.screen.height;")
-                i = 1
-                keep_scrolling = True
+                    # Scrolling implementation:
+                    # Credit: https://medium.com/analytics-vidhya/using-python-and-selenium-to-scrape-infinite-scroll-web-pages-825d12c24ec7
+                    scrolling_wait = 1
+                    website_height = driver.execute_script("return window.screen.height;")
+                    i = 1
+                    keep_scrolling = True
 
-                while keep_scrolling:
-                    driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=website_height, i=i))
-                    i += 1
-                    time.sleep(scrolling_wait)
-                    scroll_height = driver.execute_script("return document.body.scrollHeight;")
-                    if website_height * i > scroll_height:
-                        keep_scrolling = False
+                    while keep_scrolling:
+                        driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=website_height, i=i))
+                        i += 1
+                        time.sleep(scrolling_wait)
+                        scroll_height = driver.execute_script("return document.body.scrollHeight;")
+                        if website_height * i > scroll_height:
+                            keep_scrolling = False
 
-                # Parse the HTML content
-                htmlSource = driver.page_source
-                soup = BeautifulSoup(htmlSource, "html.parser")
+                    # Parse the HTML content
+                    htmlSource = driver.page_source
+                    soup = BeautifulSoup(htmlSource, "html.parser")
 
-                # Extract player prop bets data
-                results = soup.find(id="odds-app")
-                prop_list = []
-                players = results.find_all("div", class_="grouped-items-with-sticky-footer__content")
+                    # Extract player prop bets data
+                    results = soup.find(id="odds-app")
+                    prop_list = []
+                    players = results.find_all("div", class_="grouped-items-with-sticky-footer__content")
 
-                for player in players:
-                    player_name = player.find("a", class_="odds-player__heading")
-                    line = player.find("span", class_="odds-cell__line")
-                    odds = player.find("span", class_="odds-cell__cost")
-                    prop_list.append((player_name.text.strip(), line.text.strip(), odds.text.strip()))
+                    for player in players:
+                        player_name = player.find("a", class_="odds-player__heading")
+                        line = player.find("span", class_="odds-cell__line")
+                        odds = player.find("span", class_="odds-cell__cost")
+                        prop_list.append((player_name.text.strip(), line.text.strip(), odds.text.strip()))
 
-                # Write prop bets data to the output file
-                date_string = f"Date: {month}-{day}-{year}\n"
-                file.write(date_string)
+                    # Write prop bets data to the output file
+                    date_string = f"Date: {month}-{day}-{year}\n"
+                    file.write(date_string)
 
-                for line in prop_list:
-                    line_string = f"Player: {line[0]}, line: {line[1]}, odds: {line[2]}\n"
-                    file.write(line_string)
+                    for line in prop_list:
+                        line_string = f"Player: {line[0]}, line: {line[1]}, odds: {line[2]}\n"
+                        file.write(line_string)
 
-                file.write("\n")
+                    file.write("\n")
+
+# get_data("points")
+# get_data("assists")
+get_data("rebounds")
--- prop_bet_pickle.py
+++ prop_bet_pickle.py
@@ -15,21 +15,32 @@ class Stat:
             current_date = ""
             for line in lines:
                 if line[0:4] == "Date":
-                    current_date = line[6:]
+                    current_date = line[6:-1]
+                    self.player_props[current_date] = []
                 elif line[0:6] == "Player":
                     new_line = line[8:].split(", ")
-                    self.player_props[current_date] = (new_line[0], new_line[1][8:], new_line[2][7:11])
+                    self.player_props[current_date].append((new_line[0], new_line[1][8:], new_line[2][7:11]))
 
 points = Stat("points_prop_bet_output.txt")
 points.getPlayerProps()
 with open('points_prop_bets.pkl', 'wb') as outp:
     pickle.dump(points.player_props, outp, pickle.HIGHEST_PROTOCOL)
 
-points_date_dict = {}
-with open('points_prop_bets.pkl', 'rb') as inp:
-    points_date_dict = pickle.load(inp)
+assists = Stat("assists_prop_bet_output.txt")
+assists.getPlayerProps()
+with open('assists_prop_bets.pkl', 'wb') as outp:
+    pickle.dump(assists.player_props, outp, pickle.HIGHEST_PROTOCOL)
 
-print(points_date_dict['1-6-2021'])
+rebounds = Stat("rebounds_prop_bet_output.txt")
+rebounds.getPlayerProps()
+with open('rebounds_prop_bets.pkl', 'wb') as outp:
+    pickle.dump(rebounds.player_props, outp, pickle.HIGHEST_PROTOCOL)
+
+# Example Usage:
+# points_date_dict = {}
+# with open('points_prop_bets.pkl', 'rb') as inp:
+#     points_date_dict = pickle.load(inp)
+# print(points_date_dict['1-6-2021'])
                     
     
         
